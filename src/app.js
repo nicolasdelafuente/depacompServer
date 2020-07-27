33990ccfe99a11716ef14703f08173d5334432de
nicolasdelafuente <48973434+nicolasdelafuente@users.noreply.github.com>
2020-07-27 10:05:02 -0300

    CORS Configurado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ import express, { json } from "express";
 import morgan from "morgan";
 import seguimientoRoutes from "./routes/seguimientos";
 
+const cors = require("cors");
+
 //Inicializción
 const app = express();
 
 //middlewares
+app.use(cors());
 //muestro por consola lo que va llegando.
 app.use(morgan("dev"));
 //el servidor puede entender los archivos.json
