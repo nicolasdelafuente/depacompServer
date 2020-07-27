6328f1d7a5e8d2a2020835c46f70573bce5fae7e
nicolasdelafuente <48973434+nicolasdelafuente@users.noreply.github.com>
2020-07-27 08:25:35 -0300

    Modifiqué readme.md

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const app = express();
 const pool = require("./server/db.js");
 const { restart } = require("nodemon");
 
-app.use(express.json()) //es para tenerl body del json
+app.use(express.json()); //es para tenerl body del json
 
 //Routes//
 
 //Crear una linea//
-app.post("/entrevista", async(req, res) => {
-    try {
-        const {nombre, descripcion} = req.body;
-        const newEntrevista = await pool.query("INSERT INTO dpto.entrevista (nombre, descripcion) VALUES ($1, $2) RETURNING *",
-        [nombre, descripcion]
-        );
-        res.json(newEntrevista);
-
-    } catch (err) {
-        console.error(err.message)
-    }
-})
+app.post("/entrevista", async (req, res) => {
+  try {
+    const { nombre, descripcion } = req.body;
+    const newEntrevista = await pool.query(
+      "INSERT INTO dpto.entrevista (nombre, descripcion) VALUES ($1, $2) RETURNING *",
+      [nombre, descripcion]
+    );
+    res.json(newEntrevista);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // pool.connect((err,db,done)=>{
 //     if(err){
@@ -29,7 +29,7 @@ app.post("/entrevista", async(req, res) => {
 //         const nombre = "Eduardo";
 //         const descripcion = "Es el cuervo del equipo";
 
-//         db.query("INSERT INTO dpto.entrevista (nombre, descrfipcion) VALUES ($1, $2) RETURNING *", 
+//         db.query("INSERT INTO dpto.entrevista (nombre, descrfipcion) VALUES ($1, $2) RETURNING *",
 //             [nombre,descripcion]);
 //             done();
 //             if(err){
@@ -43,5 +43,5 @@ app.post("/entrevista", async(req, res) => {
 // })
 
 app.listen(5000, () => {
-    console.log("Server is listening on port 5000")
-})
+  console.log("Server is listening on port 5000");
+});
