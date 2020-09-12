2c631302eaf6d1e424c5b910dbdd8a87fd3e380a
Nicolas de la Fuente <ni.delafuente@gmail.com>
2020-09-12 12:50:18 -0300

    configurando local Host

--- src/database/database.js
+++ src/database/database.js
@@ -1,18 +1,13 @@
 import Sequelize from "sequelize";
 
-export const sequelize = new Sequelize(
-  "departamento",
-  "postgres",
-  "Admin2020",
-  {
-    host: "34.71.29.253",
-    dialect: "postgres",
-    pool: {
-      max: 5,
-      min: 0,
-      require: 30000,
-      idle: 10000,
-    },
-    logging: false,
-  }
-);
+export const sequelize = new Sequelize("departamento", "postgres", "root", {
+  host: "localhost",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    require: 30000,
+    idle: 10000,
+  },
+  logging: false,
+});
