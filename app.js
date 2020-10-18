a35c872796a38350a92e4c422ad1acb2c4808f6b
Adrian Fazio <adrian.fazio@outlook.com>
2020-10-18 00:35:29 -0300

    detalles de conexion al server

--- app.js
+++ app.js
@@ -18,17 +18,14 @@ app.use("/api/entrevistas", require("./routes/entrevista.routes"));
 app.use("/api/estados", require("./routes/estado.routes"));
 app.use("/api/orientadores", require("./routes/orientador.routes"));
 app.use("/api/personas", require("./routes/persona.routes"));
-app.use(
-  "/api/sectores_derivacion",
-  require("./routes/sectorDerivacion.routes")
-);
+app.use("/api/sectores_derivacion", require("./routes/sectorDerivacion.routes"));
 app.use("/api/seguimientos", require("./routes/seguimiento.routes"));
 app.use("/api/tipos_documento", require("./routes/tipoDocumento.routes"));
 app.use("/api/tipos_seguimiento", require("./routes/tipoSeguimiento.routes"));
 
 //Arranque de servidor
 app.listen(PORT, function () {
-  console.log(`La app arranca en http://localhost:${PORT}`);
+  console.log(`La app arranca en http://35.247.230.28:${PORT}`);
 
   // Conectase a la base de datos
   // Force true: DROP TABLES
